83ee4c99f9f9d345e5721d9248fc48a3b9711e8b
DESKTOP-C5Q0O6U\MY RAFI <firzassss@gmail.com>
2023-10-23 12:13:22 +0700

    menambah code awal kasus liskov

---
+++ lsp/Smartphone.java
@@ -0,0 +1,16 @@
+package lsp;
+
+import java.sql.Date;
+
+public class Smartphone extends Product {
+    @Override
+    String getName() {
+        return "Samsung S22 Ultra";
+    }
+
+    @Override
+    Date getExpiredDate() {
+        return new Date(0);
+    }
+
+}
